9f47053d732e579435bca54ceb59e7a46478457a
Sagar Kuvar <skuvar4@gmail.com>
2021-12-06 00:16:50 +0530

    updated AddOneToNumber.py
    updated AddOneToNumber.py

--- arrays/AddOneToNumber.py
+++ arrays/AddOneToNumber.py
@@ -1,4 +1,4 @@
-/*
+'''
 Given a non-negative number represented as an array of digits,
 add 1 to the number ( increment the number represented by the digits ).
 The digits are stored such that the most significant digit is at the head of the list.
@@ -12,8 +12,8 @@ Q : Can the input have 0’s before the most significant digit. Or in other word
 A : For the purpose of this question, YES
 Q : Can the output have 0’s before the most significant digit? Or in other words, is 0 1 2 4 a valid output?
 A : For the purpose of this question, NO. Even if the input has zeroes before the most significant digit.
-*/
 
+'''
 solution 
 
 a=[9,9,9]
